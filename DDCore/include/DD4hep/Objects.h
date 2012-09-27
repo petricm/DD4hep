313c218152a116892036c1773c438ec0fa4a2cf7
Markus Frank <markus.frank@cern.ch>
2012-09-27 14:07:01 +0000

    Bug fixes

--- DDCore/include/DD4hep/Objects.h
+++ DDCore/include/DD4hep/Objects.h
@@ -154,6 +154,13 @@ namespace DD4hep {
           return *this;
         }
       }
+      /// Rotates the position vector around the x-axis.
+      Position& rotateX(double angle_in_rad);
+      /// Rotates the position vector around the y-axis.
+      Position& rotateY(double angle_in_rad);
+      /// Rotates the position vector around the z-axis.
+      Position& rotateZ(double angle_in_rad);
+
       /// Access to array like coordinates
       const double* coordinates() const { return &x; }
       /// Initializer for all member variables
@@ -214,6 +221,12 @@ namespace DD4hep {
         psi   = ps;
         return *this;
       }
+      /// Rotates the rotation vector around the x-axis.
+      Rotation& rotateX(double angle_in_rad);
+      /// Rotates the rotation vector around the y-axis.
+      Rotation& rotateY(double angle_in_rad);
+      /// Rotates the rotation vector around the z-axis.
+      Rotation& rotateZ(double angle_in_rad);
     };
 
 #ifdef _WIN32
