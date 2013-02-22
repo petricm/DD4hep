2775243c8f7ac5364ed74bd06280768abbd4763c
Markus Frank <markus.frank@cern.ch>
2013-02-22 17:15:06 +0000

    Have a common representation for all unicode macros.

---
+++ DDExamples/UtilityApps/src/run_plugin.h
@@ -0,0 +1,36 @@
+// $Id:$
+//====================================================================
+//  AIDA Detector description implementation for LCD
+//--------------------------------------------------------------------
+//
+//  Generic ROOT based geometry display program
+//
+//  Author     : M.Frank
+//
+//====================================================================
+
+// Framework include files
+#include "DD4hep/LCDD.h"
+
+// C/C++ include files
+#include <cerrno>
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+using namespace DD4hep::Geometry;
+
+int run_plugin(LCDD& lcdd, const char* name, int argc, char** argv) {
+  try {
+    lcdd.apply(name, argc, argv);
+    return 0;
+  } catch (const exception& e) {
+    cout << e.what() << endl;
+  } catch (...) {
+    cout << "UNKNOWN Exception" << endl;
+  }
+  ::exit(EINVAL);
+  return EINVAL;
+}
