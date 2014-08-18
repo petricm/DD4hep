ed73d041433b66f03908285e08b67e99442ca8d7
Markus Frank <markus.frank@cern.ch>
2014-08-18 12:26:27 +0000

    Remove dead bodies from the repository

--- DDEve/include/DDEve/Dictionary.h
+++ DDEve/include/DDEve/Dictionary.h
@@ -10,31 +10,25 @@
 #define DD4HEP_DDEVE_DICTIONARY_H
 
 // Framework include files
-#include "DDEve/Annotation.h"
-#include "DDEve/BasicView.h"
 #include "DDEve/Calo2DProjection.h"
 #include "DDEve/Calo3DProjection.h"
 #include "DDEve/CaloLego.h"
-#include "DDEve/ContextMenu.h"
-#include "DDEve/DD4hepMenu.h"
 #include "DDEve/Display.h"
-#include "DDEve/ElementList.h"
-#include "DDEve/EmbeddedView.h"
-#include "DDEve/EvePgonSetProjectedContextMenu.h"
-#include "DDEve/EveShapeContextMenu.h"
 #include "DDEve/EventControl.h"
 #include "DDEve/EventHandler.h"
 #include "DDEve/MultiView.h"
-#include "DDEve/ProjectionView.h"
 #include "DDEve/RhoPhiProjection.h"
 #include "DDEve/RhoZProjection.h"
-#include "DDEve/SplitView.h"
-#include "DDEve/ToolTip.h"
 #include "DDEve/View3D.h"
-#include "DDEve/ViewMenu.h"
-//#include "DDEve/.h"
+
+#include "DDEve/Annotation.h"
+#include "DDEve/ContextMenu.h"
+#include "DDEve/DD4hepMenu.h"
 #include "DDEve/DisplayConfiguration.h"
-#include "TEveElement.h"
+#include "DDEve/ElementList.h"
+#include "DDEve/EvePgonSetProjectedContextMenu.h"
+#include "DDEve/EveShapeContextMenu.h"
+#include "DDEve/ViewMenu.h"
 
 #include "DDEve/DDG4EventHandler.h"
 #include "DDEve/GenericEventHandler.h"
@@ -53,23 +47,15 @@ namespace DD4hep {
 #pragma link C++ function DD4hep::EveDisplay(const char* xmlFile);
 
 #pragma link C++ class DD4hep::Display;
-#pragma link C++ class DD4hep::ToolTip;
-#pragma link C++ class DD4hep::BasicView;
-#pragma link C++ class DD4hep::SplitView;
 #pragma link C++ class DD4hep::Annotation;
-#pragma link C++ class DD4hep::MultiView;
-#pragma link C++ class DD4hep::ProjectionView;
 #pragma link C++ class DD4hep::ElementList;
-#pragma link C++ class DD4hep::PopupMenu;
 #pragma link C++ class DD4hep::FrameControl;
 #pragma link C++ class DD4hep::EventHandler;
 #pragma link C++ class DD4hep::EventConsumer;
 #pragma link C++ class DD4hep::DDG4EventHandler;
 #pragma link C++ class DD4hep::GenericEventHandler;
 #pragma link C++ class DD4hep::EventControl;
-#pragma link C++ class DD4hep::EmbeddedView;
 
-#pragma link C++ class DD4hep::DD4hepMenu;
 #pragma link C++ class DD4hep::DisplayConfiguration::Config;
 //#pragma link C++ class std::list<DD4hep::DisplayConfiguration::Config>;
 #pragma link C++ class DD4hep::DisplayConfiguration::ViewConfig;
@@ -77,7 +63,6 @@ namespace DD4hep {
 #pragma link C++ class DD4hep::DisplayConfiguration;
 
 #pragma link C++ class DD4hep::View;
-#pragma link C++ class DD4hep::ViewMenu;
 #pragma link C++ class DD4hep::View3D;
 #pragma link C++ class DD4hep::Projection;
 #pragma link C++ class DD4hep::RhoZProjection;
@@ -85,6 +70,11 @@ namespace DD4hep {
 #pragma link C++ class DD4hep::CaloLego;
 #pragma link C++ class DD4hep::Calo2DProjection;
 #pragma link C++ class DD4hep::Calo3DProjection;
+#pragma link C++ class DD4hep::MultiView;
+
+#pragma link C++ class DD4hep::PopupMenu;
+#pragma link C++ class DD4hep::ViewMenu;
+#pragma link C++ class DD4hep::DD4hepMenu;
 #pragma link C++ class DD4hep::ContextMenu;
 #pragma link C++ class DD4hep::ContextMenuHandler;
 #pragma link C++ class DD4hep::EveUserContextMenu;
