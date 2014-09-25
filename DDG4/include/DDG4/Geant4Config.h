5bf9def154c38a862fb627d1b5f601aa39177677
Markus Frank <markus.frank@cern.ch>
2014-09-25 09:11:14 +0000

    Intermediate chick-in for Frank

--- DDG4/include/DDG4/Geant4Config.h
+++ DDG4/include/DDG4/Geant4Config.h
@@ -86,6 +86,7 @@ namespace DD4hep {
 #include "DDG4/Geant4GeneratorAction.h"
 #include "DDG4/Geant4Handle.h"
 #include "DDG4/Geant4Kernel.h"
+#include "DDG4/Geant4Particle.h"
 #include "DDG4/Geant4ParticleHandler.h"
 #include "DDG4/Geant4PhysicsList.h"
 #include "DDG4/Geant4RunAction.h"
