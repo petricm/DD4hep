e9387242577ecf4a00c326e0a5b6f7eb40975df8
Markus Frank <markus.frank@cern.ch>
2014-09-26 19:58:37 +0000

    Fix MC truth handling

--- DDG4/include/DDG4/Geant4MonteCarloTruth.h
+++ DDG4/include/DDG4/Geant4MonteCarloTruth.h
@@ -36,11 +36,6 @@ namespace DD4hep {
      *  \ingroup DD4HEP_SIMULATION
      */
     class Geant4MonteCarloTruth {
-    public:
-      typedef Geant4Particle Particle;
-      typedef std::map<int, Particle*> ParticleMap;
-      typedef std::map<int, int>       TrackEquivalents;
-
     protected:
       /// Standard constructor
       Geant4MonteCarloTruth();
@@ -48,14 +43,6 @@ namespace DD4hep {
     public:
       /// Default destructor
       virtual ~Geant4MonteCarloTruth();
-#ifndef __MAKECINT__
-      /// Access the particle map
-      virtual const ParticleMap& particles() const = 0;
-      /// Access the map of track equivalents
-      virtual const TrackEquivalents& equivalents() const = 0;
-#endif
-      /// Access the equivalent track id (shortcut to the usage of TrackEquivalents)
-      virtual int particleID(int track, bool throw_if_not_found = true) const = 0;
       /// Mark a Geant4 track to be kept for later MC truth analysis
       virtual void mark(const G4Track* track) = 0;
       /// Store a track, with a flag
@@ -73,25 +60,11 @@ namespace DD4hep {
      *  \ingroup DD4HEP_SIMULATION
      */
     class Geant4DummyTruthHandler : public Geant4Action, public Geant4MonteCarloTruth {
-    protected:
-      /// Map with stored MC Particles
-      ParticleMap m_particleMap;
-      /// Map associating the G4Track identifiers with identifiers of existing MCParticles
-      TrackEquivalents m_equivalentTracks;
-
     public:
       /// Standard constructor
       Geant4DummyTruthHandler(Geant4Context* ctxt, const std::string& nam);
       /// Default destructor
       virtual ~Geant4DummyTruthHandler();
-#ifndef __MAKECINT__
-      /// Access the particle map
-      virtual const ParticleMap& particles() const { return m_particleMap; }
-      /// Access the map of track equivalents
-      virtual const TrackEquivalents& equivalents() const { return m_equivalentTracks; }
-#endif
-      /// Access the equivalent track id (shortcut to the usage of TrackEquivalents)
-      virtual int particleID(int track, bool) const { return track; }
       /// Mark a Geant4 track to be kept for later MC truth analysis. Default flag: CREATED_HIT
       virtual void mark(const G4Track* track);
       /// Store a track, with a flag
