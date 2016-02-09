7d7131ff56b9a313741efaa023cd7bbd2c5e52e8
Frank Gaede <frank.gaede@desy.de>
2016-02-09 09:52:40 +0000

    - renamed EMCALO, HADCALo to ELECTROMAGNETIC, HADRONIC in DetTypes  - use DetectorSelector in dumpdetector

--- DDCore/include/DD4hep/DetType.h
+++ DDCore/include/DD4hep/DetType.h
@@ -21,7 +21,7 @@ namespace DD4hep {
   /** Helper class for encoding sub detector types in a flag word.
    *  Example:<br>
    *  DetType type( DetType::TRACKER | DetType::STRIP | DetType::BARREL ) ; <br>
-   *  type.is( DetType::EMCALO  ) ; // false <br>
+   *  type.is( DetType::ELECTROMAGNETIC  ) ; // false <br>
    *  type.isNot( DetType::CALORIMETER  ) ; // true <br>
    *  type.is( DetType::STRIP | DetType::BARREL ) ; // true <br>
    *
@@ -34,22 +34,22 @@ namespace DD4hep {
 
   public:
     enum {
-      IGNORE      = 0,
-      TRACKER     = 1 << 0,
-      CALORIMETER = 1 << 1,
-      CHERENKOV   = 1 << 2,
-      ENDCAP      = 1 << 3,
-      BARREL      = 1 << 4,
-      FORWARD     = 1 << 5,
-      VERTEX      = 1 << 6,
-      STRIP       = 1 << 7,
-      PIXEL       = 1 << 8,
-      GASEOUS     = 1 << 9,
-      WIRE        = 1 << 10,
-      EMCALO      = 1 << 11,
-      HADCALO     = 1 << 12,
-      MUON        = 1 << 13,
-      SUPPORT     = 1 << 14
+      IGNORE          = 0,
+      TRACKER         = 1 << 0,
+      CALORIMETER     = 1 << 1,
+      CHERENKOV       = 1 << 2,
+      ENDCAP          = 1 << 3,
+      BARREL          = 1 << 4,
+      FORWARD         = 1 << 5,
+      VERTEX          = 1 << 6,
+      STRIP           = 1 << 7,
+      PIXEL           = 1 << 8,
+      GASEOUS         = 1 << 9,
+      WIRE            = 1 << 10,
+      ELECTROMAGNETIC = 1 << 11,
+      HADRONIC        = 1 << 12,
+      MUON            = 1 << 13,
+      SUPPORT         = 1 << 14
     };
 
     /// default c'tor
@@ -103,10 +103,10 @@ namespace DD4hep {
       os << "GASEOUS, ";
     if (t.is(DetType::WIRE))
       os << "WIRE, ";
-    if (t.is(DetType::EMCALO))
-      os << "EMCALO, ";
-    if (t.is(DetType::HADCALO))
-      os << "HADCALO, ";
+    if (t.is(DetType::ELECTROMAGNETIC))
+      os << "ELECTROMAGNETIC, ";
+    if (t.is(DetType::HADRONIC))
+      os << "HADRONIC, ";
     if (t.is(DetType::MUON))
       os << "MUON, ";
     if (t.is(DetType::SUPPORT))
