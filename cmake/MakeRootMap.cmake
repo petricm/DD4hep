d2e5f7d87a432bac5a7b1960cb1d04d8c9b5d111
Marko Petric <marko.petric@cern.ch>
2016-11-27 19:29:49 +0100

    Addopt to the new FindROOT and use the use file of ROOT

--- cmake/MakeRootMap.cmake
+++ cmake/MakeRootMap.cmake
@@ -1,11 +1,11 @@
-message(STATUS " *** ROOT_genmap[Root version: ${ROOT_VERSION_MAJOR}]: Generate map for ${libname} ..." )
-if( ${ROOT_VERSION_MAJOR} GREATER 5 )
+message(STATUS " *** ROOT_genmap[Root version: ${ROOT_VERSION}]: Generate map for ${libname} ..." )
+if( ${ROOT_VERSION} GREATER 5 )
   #
   #  Running listcomponents from Gaudi
   #   
   FIND_PROGRAM( ROOT_genmap_CMD listcomponents PATHS ${genmap_install_dir}/../bin ${DD4hep_DIR}/bin )
   ###MESSAGE( STATUS " *** MakeRootMap ${genmap_install_dir}/../bin $ENV{DD4hep_DIR}/bin ${DD4hep_DIR}" )
-  MESSAGE( STATUS " *** MakeRootMap[${ROOT_VERSION_MAJOR}].cmake run command : ${ROOT_genmap_CMD} -o ${rootmapfile} ${libname} 
+  MESSAGE( STATUS " *** MakeRootMap[${ROOT_VERSION}].cmake run command : ${ROOT_genmap_CMD} -o ${rootmapfile} ${libname}
                  WORKING_DIRECTORY ${genmap_install_dir} "
     ) 
 
@@ -25,7 +25,7 @@ else()
   #  Running genmap from ROOT 5
   #
   FIND_PROGRAM( ROOT_genmap_CMD genmap PATHS $ENV{ROOTSYS}/bin $ENV{PATH} )
-  MESSAGE( STATUS " *** MakeRootMap[${ROOT_VERSION_MAJOR}].cmake run command : ${ROOT_genmap_CMD} -debug -i ${libname} -o ${rootmapfile}
+  MESSAGE( STATUS " *** MakeRootMap[${ROOT_VERSION}].cmake run command : ${ROOT_genmap_CMD} -debug -i ${libname} -o ${rootmapfile}
                  WORKING_DIRECTORY ${genmap_install_dir} "
     )
 
