7342474c71c6d333d8c493bb62825e647a46bdc2
Marko Petric <marko.petric@cern.ch>
2019-07-05 13:32:13 +0200

    tesT

--- .dd4hep-ci.d/compile_and_test.sh
+++ .dd4hep-ci.d/compile_and_test.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-source /DD4hep/.dd4hep-ci.d/init_lcg.sh
-
-cd /DD4hep
-mkdir build
-cd build
-cmake -GNinja -DDD4HEP_USE_GEANT4=ON -DBoost_NO_BOOST_CMAKE=ON -DDD4HEP_USE_LCIO=ON -DBUILD_TESTING=ON -DGeant4_DIR=$G4INSTALL/lib64/Geant4-10.5.0 -DCMAKE_BUILD_TYPE=Release -DROOT_DIR=$ROOTSYS -DCMAKE_CXX_FLAGS="-fdiagnostics-color=always -Werror" .. && \
+source /DD4hep/.dd4hep-ci.d/init_lcg.sh && \
+cd /DD4hep && \
+mkdir build && \
+cd build && \
+cmake -GNinja -DDD4HEP_USE_GEANT4=ON -DBoost_NO_BOOST_CMAKE=ON -DDD4HEP_USE_LCIO=OFF -DBUILD_TESTING=ON -DGeant4_DIR=$G4INSTALL/lib64/Geant4-10.5.0 -DCMAKE_BUILD_TYPE=Release -DROOT_DIR=$ROOTSYS -DCMAKE_CXX_FLAGS="-fdiagnostics-color=always -Werror" .. && \
 ninja && \
 ninja install && \
 . ../bin/thisdd4hep.sh && \
